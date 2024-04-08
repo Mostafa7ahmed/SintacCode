6613435075d9592abb098e976e19f43463afb67e
mostafa hamed <154756540+Mostafa7ahmed@users.noreply.github.com>
2024-04-08 13:44:42 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+
+let sub_nav = document.querySelectorAll(".sub-nav")[0];
+let nav_media = document.querySelectorAll(".navMidia")[0];
+nav_media.onclick = function(){
+    sub_nav.classList.toggle('ds-b');
+
+}
